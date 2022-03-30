34f1e36af7e232255dbccdc7987c73f73706a064
pousada <naldojrs@gmail.com>
2022-03-30 13:57:38 -0300

    Create Project

---
+++ src/test/java/com/naldojrs/bootspring/BootspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naldojrs.bootspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
